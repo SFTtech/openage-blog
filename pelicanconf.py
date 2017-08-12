a4b05318ec5a0d391000179ae8db8eb44779bdbf
Jonas Jelten <jj@sft.mx>
2017-08-12 02:56:52 +0200

    initial pelican config

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+AUTHOR = "' OR 1=1; DROP TABLE blogposts; --"
+SITENAME = 'openage blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('openage', 'http://openage.sft.mx/'),
+    ('source code', 'https://github.com/SFTtech/openage/'),
+    ('blog origin', 'https://github.com/SFTtech/openage-blog/'),
+)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
