96129eabb1e05c6b1a32c8e2180a2a5e12fcbef6
Jonas Jelten <jj@sft.mx>
2019-06-26 21:58:14 +0200

    fix publish configuration

--- publishconf.py
+++ publishconf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.append(os.curdir)
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from pelicanconf import *
 
 SITEURL = 'https://blog.openage.sft.mx'
